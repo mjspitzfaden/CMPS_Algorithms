7c27f6ae7277b879cee9f86a1be0e83517f908a9
Michael Spitzfaden <31863270+mjspitzfaden@users.noreply.github.com>
2017-11-24 08:48:31 -0600

    bubble sort

---
+++ bubble.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+int hold;
+int array[5];
+cout<<"Enter 10 numbers: "<<endl;
+for(int i=0; i<10; i++)
+{
+cin>>array[i];
+}
+cout<<endl;
+cout<<"Orignally entered array by the user is: "<<endl;
+
+//Bubble sort vale of n^2
+for(int j=0; j<5; j++)
+{
+cout<<array[j];
+cout<<endl;
+}
+cout<<endl;
+for(int i=0; i<4; i++)
+{
+  for(int j=0; j<4; j++)
+  {
+    if(array[j]>array[j+1])
+    {
+      hold=array[j];
+      array[j]=array[j+1];
+      array[j+1]=hold;
+    }
+  }
+}
+cout<<"Sorted Array is: "<<endl;
+for(int i=0; i<5; i++)
+{
+cout<<array[i]<<endl;
+}
+
+return 0;
+}
